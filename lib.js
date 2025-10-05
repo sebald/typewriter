0b3bdcecab068dc70f952c45e05b085e6db906fe
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-10-05 22:58:36 +0200

    save

--- lib.js
+++ lib.js
@@ -1,43 +1,45 @@
 // Helpers
 // ---------------
-const DEFAULT_TYPING_DELAYS = [50, 75, 100, 150, 200];
+const TYPING_DELAYS = [50, 75, 100, 150, 200];
+const FIELD_DELAYS = [300, 400, 500];
 
-const getRandomDelay = () => {
-  const randomIndex = Math.floor(Math.random() * DEFAULT_TYPING_DELAYS.length);
-  return DEFAULT_TYPING_DELAYS[randomIndex];
+const pick = array => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 };
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const typeIntoInput = async (elementId, text) => {
-  const inputField = document.getElementById(elementId);
+  const el = document.getElementById(elementId);
 
-  if (!inputField) {
+  if (!el) {
     throw new Error(`Input element with ID '${elementId}' not found`);
   }
 
-  inputField.value = '';
+  el.value = '';
 
   for (const char of text) {
-    inputField.value += char;
-    inputField.dispatchEvent(new Event('input', { bubbles: true }));
-    await delay(getRandomDelay());
+    el.value += char;
+    el.dispatchEvent(new Event('input', { bubbles: true }));
+    await delay(pick(TYPING_DELAYS));
   }
 };
 
-export const typewriter = async dataArray => {
-  for (let i = 0; i < dataArray.length; i++) {
-    const item = dataArray[i];
-    const inputField = document.getElementById(item.id);
+export const typewriter = async list => {
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i];
+    const el = document.getElementById(item.id);
 
-    if (!inputField) {
+    if (!el) {
       console.error(`Input element with ID '${item.id}' not found. Skipping.`);
       continue;
     }
 
-    inputField.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    inputField.focus();
+    el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    el.focus();
 
     await typeIntoInput(item.id, item.text);
+    await delay(pick(FIELD_DELAYS));
   }
 };
